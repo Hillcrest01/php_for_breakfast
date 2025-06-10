6682d9d16789c9f5f6846c6670124008da80d249
Hillcrest01 <peteromondi008@gmail.com>
2025-06-10 11:12:48 +0300

    add the deletion and creation functionalities

--- 22 product_crud.php
+++ 22 product_crud.php
@@ -58,13 +58,16 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     <?php foreach($products as $i => $product) {?>
             <tr>
       <th scope="row"> <?php echo $i + 1 ?> </th>
-      <td></td>
+      <td> <img src="<?php echo $product['image'] ?>" alt="product image here" class="product-image"> </td>
       <td> <?php echo $product['title'] ?> </td>
       <td> <?php echo $product['price'] ?> </td>
       <td> <?php echo $product['create_date'] ?> </td>
       <td>
         <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
-        <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+        <form style="display:inline-block" action="delete_product.php" method="post">
+          <input type="hidden" name="id" value="<?php echo $product['id'] ?>">
+          <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+        </form>
       </td>
     </tr>
     <?php } ?>
--- create_product.php
+++ create_product.php
@@ -3,11 +3,18 @@
 //for the comments refer to product_crud.php
 
 //create an instance of PDO and perform the database connection configuration
-$pdo = new PDO('mysql:host=localhost;dbname=products_crud' , 'root', 'Kulundeng.Jamach.1');
+$pdo = new PDO('mysql:host=localhost;dbname=products_crud', 'root', 'Kulundeng.Jamach.1');
 
 //in case of an error
 
-$pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+// echo '<pre>';
+// var_dump($_FILES);
+// echo '</pr>';
+
+
 
 $errors = [];
 
@@ -15,39 +22,73 @@ $price = '';
 $description = '';
 $title = '';
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-$title = $_POST['title'];
-$description = $_POST["description"];
-$price = $_POST["price"];
-$date = date('Y-m-d H:i:s');
+  $title = $_POST['title'];
+  $description = $_POST["description"];
+  $price = $_POST["price"];
+  $date = date('Y-m-d H:i:s');
 
-if(!$title){
-  $errors[] = 'Please provide a title';
-}
-if(!$description){
-  $errors[] = 'Please provide a description';
-}
-if(!$price){
-  $errors[] = 'Please provide a price';
-}
+  if (!$title) {
+    $errors[] = 'Please provide a title';
+  }
+  if (!$description) {
+    $errors[] = 'Please provide a description';
+  }
+  if (!$price) {
+    $errors[] = 'Please provide a price';
+  }
+
+  if (!is_dir('images')) {
+    mkdir('images');
+  }
+  if (empty($errors)) {
 
 
-if(empty($errors)){
-$statement = $pdo->prepare("INSERT INTO product (title, description, image, price, create_date, other_comments)
+    $image = $_FILES['image'] ?? null;
+    $imagePath = '';
+
+
+    //We check if the image has a tmp_name to ensure only if the image is uploaded is when we store it to our backend
+    if ($image && $image['tmp_name']) {
+
+      $imagePath = 'images/' . randomString(8) . '/' . $image['name'];
+      mkdir(dirname($imagePath));
+      echo $imagePath;
+
+      move_uploaded_file($image['tmp_name'], $imagePath);
+    }
+  
+
+
+    $statement = $pdo->prepare("INSERT INTO product (title, description, image, price, create_date, other_comments)
               VALUES (:title, :description, :image, :price ,:date, :other_comments )
               ");
 
-$statement->bindValue(':title' , $title);
-$statement->bindValue(':description' , $description);
-$statement->bindValue(':image' , '');
-$statement->bindValue(':price' , $price);
-$statement->bindValue(':date' , $date);
-$statement->bindValue(':other_comments' , '');
-$statement->execute();
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':image', $imagePath);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':date', $date);
+    $statement->bindValue(':other_comments', '');
+    $statement->execute();
+    header('Location: 22 product_crud.php');
+  }
+}
 
+function randomString($n)
+{
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $str = '';
+
+  for ($i = 0; $i < $n; $i++) {
+    $index = rand(0, strlen($characters) - 1);
+    $str .= $characters[$index];
+  }
+  return $str;
 }
-}
+
+
 
 ?>
 
@@ -55,50 +96,53 @@ $statement->execute();
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>create a product</title>
-    <link rel="stylesheet" href="index.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>List of Products</h1>
-  
-      <?php if(!empty($errors)): ?>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>create a product</title>
+  <link rel="stylesheet" href="index.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+
+<body>
+  <h1>List of Products</h1>
+
+  <?php if (!empty($errors)): ?>
     <div class="alert alert-danger">
-      <?php foreach($errors as $error) : ?>
+      <?php foreach ($errors as $error) : ?>
         <div> <?php echo $error ?> </div>
-      
+
       <?php endforeach; ?>
 
     </div>
-    <?php endif; ?>
-        
-    <form action="create_product.php" method="post">
-        <div class="form-group">
-            <label for="image"> Product Image: </label> <br>
-            <input type="file" name="image"> 
-        </div>&nbsp;
-  <div class="mb-3">
-    <label for="product" class="form-label">Product Title:</label>
-    <input type="text" name="title" class="form-control" value= <?php echo $title ?> >
-  </div>
+  <?php endif; ?>
+
+  <form action="create_product.php" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+      <label for="image"> Product Image: </label> <br>
+      <input type="file" name="image">
+    </div>&nbsp;
+    <div class="mb-3">
+      <label for="product" class="form-label">Product Title:</label>
+      <input type="text" name="title" class="form-control" value=<?php echo $title ?>>
+    </div>
 
     <div class="mb-3">
-    <label for="product" class="form-label">Product Description:</label>
-    <textarea  name="description" class="form-control" value= <?php echo $description ?>></textarea>
-  </div>
+      <label for="product" class="form-label">Product Description:</label>
+      <textarea name="description" class="form-control" value=<?php echo $description ?>></textarea>
+    </div>
 
-      <div class="mb-3">
-    <label for="product" class="form-label">Product Price:</label>
-    <textarea name="price" type="text"  class="form-control" value= <?php echo $price ?>></textarea>
-  </div>
+    <div class="mb-3">
+      <label for="product" class="form-label">Product Price:</label>
+      <textarea name="price" type="text" class="form-control" value=<?php echo $price ?>></textarea>
+    </div>
 
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+
 </html>
